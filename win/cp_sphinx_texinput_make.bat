3525fa6ba20092647420f65c8a073bcd749d32c1
Yusuke Ishida <yusuke.ishida.gnl@gmail.com>
2015-07-21 22:21:59 +0900

    Sphinxのlatex用makeファイルの配置先を変更

--- win/cp_sphinx_texinput_make.bat
+++ win/cp_sphinx_texinput_make.bat
@@ -3,6 +3,6 @@
 @rem そこで、まずカレントディレクトリを移動しないと想定どおりの動作とならないので注意！
 cd /d %~dp0
 
-@rem SphinxInstaller-1.2.3.20141115-py2.7-win32.zip では下記パスでOK
-copy sphinx_texinput_make.bat "%ProgramFiles(x86)%\Sphinx\eggs\sphinx-1.2.3-py2.7.egg\sphinx\texinputs\make.bat"
+@rem SphinxInstaller-1.3b1.20141120-py2.7-win32.zip では下記パスでOK
+copy sphinx_texinput_make.bat "%ProgramFiles(x86)%\Sphinx\eggs\sphinx-1.3b1-py2.7.egg\sphinx\texinputs\make.bat"
 
